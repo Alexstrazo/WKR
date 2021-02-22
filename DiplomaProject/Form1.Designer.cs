95fabfeb4f7a42555b70c2d9af65d90ee56bb0bd
Alexstrazo <leonov.alexandr.1995@gmail.com>
2021-02-22 13:51:15 +0300

    upd

--- DiplomaProject/Form1.Designer.cs
+++ DiplomaProject/Form1.Designer.cs
@@ -57,7 +57,7 @@ namespace DiplomaProject
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(577, 117);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Ученик (В разработке)";
+            this.button2.Text = "Ученик";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
